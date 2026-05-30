5657e612045cd2409edc41fe4c10c5d789ba585c
Prince Narteh <prinarteh@gmail.com>
2026-05-30 12:47:21 +0000

    feat: add drizzle config

--- apps/auth-service/src/db/db.module.ts
+++ apps/auth-service/src/db/db.module.ts
@@ -1,10 +1,11 @@
 import { Pool } from 'pg';
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
+import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import * as schema from './schema';
 
 export const DRIZZLE = Symbol('drizzle-connection');
+export type DrizzleDB = NodePgDatabase<typeof schema>;
 
 @Module({
   providers: [
